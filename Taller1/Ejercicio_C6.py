0832f2c80837ad3de35a57b06698666b1ac3656b
Diana-Paola03 <dianap.avendanod@ecci.edu.co>
2024-02-28 18:53:49 -0500

    Cambio de readme

--- Taller1/Ejercicio_C6.py
+++ Taller1/Ejercicio_C6.py
@@ -1,27 +1,50 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def obtener_contornos(imagen_path):
     imagen = cv2.imread(imagen_path)
+
+    if imagen is None:
+        print(f"No se pudo cargar la imagen en la ruta: {imagen_path}")
+        return [], None
+
     gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     _, umbral = cv2.threshold(gris, 240, 255, cv2.THRESH_BINARY)
     contornos, _ = cv2.findContours(umbral, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contornos_xy = []
+
     for contorno in contornos:
         xy = np.squeeze(contorno)
         contornos_xy.append(xy)
-    return contornos_xy
+
+    return contornos_xy, imagen
 
 def main():
-    logos = ["chevrolet_logo.png", "hyundai_logo.png", "mazda_logo.png", "toyota_logo.png", "audi_logo.png"]
-    for logo in logos:
-        contornos = obtener_contornos(logo)
-        print(f"Coordenadas de contornos del logo {logo[:-4]}:")
+    imagenes = [
+        'C:/Users/diana/Pictures/ejercicio_6/Renault.PNG',
+        'C:/Users/diana/Pictures/ejercicio_6/mazda png.png' 
+        #'C:/Users/diana/Pictures/ejercicio_6/renault-logo.png'
+    ]
+
+    for imagen_path in imagenes:
+        contornos, imagen = obtener_contornos(imagen_path)
+        nombre_imagen = imagen_path.split('/')[-1].split('.')[0]
+        print(f"Coordenadas de contornos de la imagen {nombre_imagen}:")
+
         for i, contorno in enumerate(contornos):
             print(f"Contorno {i + 1}:")
             for x, y in contorno:
                 print(f"X: {x}, Y: {y}")
-        print("")
+
+        # Dibujar contornos en la imagen
+        cv2.drawContours(imagen, contornos, -1, (0, 255, 0), 2)
+
+        # Mostrar la imagen con contornos
+        plt.imshow(cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB))
+        plt.title(f'Contornos de la imagen {nombre_imagen}')
+        plt.axis('off')
+        plt.show()
 
 if __name__ == "__main__":
     main()
